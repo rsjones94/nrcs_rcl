b8a3364d4c75da0e0891752ab5113da383e86a24
rsjones94 <rsajones94@gmail.com>
2019-11-18 17:02:27 -0600

    initial commit

---
+++ nrcs_rcl.py
@@ -0,0 +1,7 @@
+"""
+The RCL (Riparian Classification from LidAR) is an Arcpy tool that creates a landcover classification raster using an
+ESRI LAS dataset (.lasd) as its input.
+"""
+
+import arcpy
+
